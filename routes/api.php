fe0fae0fd4edcf6d54cdb5891ff318b72525b381
feyswall <vicentmkota91@gmail.com>
2022-05-07 15:44:17 +0300

    first app commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\CountriesController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// public routes
+
+
+// protected routes
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('/country', CountriesController::class );
+});
+
